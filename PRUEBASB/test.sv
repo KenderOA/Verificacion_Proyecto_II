fea96caf9e39eebab651f3afb2cc38188c5bad88
amartinez102015 <110202568+AleMarAlv2018133224@users.noreply.github.com>
2024-11-15 19:01:31 -0600

    Update test.sv

--- PRUEBASB/test.sv
+++ PRUEBASB/test.sv
@@ -27,25 +27,25 @@ class test extends uvm_test;
 
         phase.raise_objection(this);
 
-		//seq.case_type = "default";
-      	//seq.randomize();
-        //seq.start(e0.a0.s0);
+	seq.case_type = "default";
+      	seq.randomize();
+        seq.start(e0.a0.s0);
       	
-      	//seq.case_type = "NaN";
-      	//seq.randomize();
-        //seq.start(e0.a0.s0);
+      	seq.case_type = "NaN";
+      	seq.randomize();
+        seq.start(e0.a0.s0);
       
       	seq.case_type = "r_invalid";
       	seq.randomize();
         seq.start(e0.a0.s0);
       
-      	//seq.case_type = "Zero";
-      	//seq.randomize();
-        //seq.start(e0.a0.s0);
+      	seq.case_type = "Zero";
+      	seq.randomize();
+        seq.start(e0.a0.s0);
       
-      	//seq.case_type = "Infinity";
-      	//seq.randomize();
-        //seq.start(e0.a0.s0);
+      	seq.case_type = "Infinity";
+      	seq.randomize();
+        seq.start(e0.a0.s0);
       
         #700;
         phase.drop_objection(this);
